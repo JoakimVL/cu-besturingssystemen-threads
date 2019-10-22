00d11e849c451e1ae4f6854a35e119d2f8629758
maximlesy <maximlesy@gmail.com>
2019-10-22 18:28:16 +0200

    Add project files.

---
+++ ThreadExample/CustomTimer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExample
+{
+
+    public delegate void TimerTickedEventHandler(object sender, int currentTime);
+
+    public class CustomTimer
+    {
+        private int currentTime;
+        private Thread timerThread;
+        private bool running;
+
+        public event TimerTickedEventHandler OnTicked;
+
+        public CustomTimer()
+        {
+            currentTime = 0;
+            running = false;
+        }
+
+        public void Start()
+        {
+            if (timerThread == null) 
+            {
+                running = true;
+                timerThread = new Thread(() => { TimerProcedure(); });
+                timerThread.Start();
+            }            
+        }
+
+        public void Stop()
+        {
+            //niet aanbevolen! Misschien onderbreek je de thread midden in zijn taak!
+            //gooit een exception
+            //if (timerThread != null) timerThread.Abort();
+            //timerThread = null;
+
+            running = false;
+            timerThread = null;
+        }
+
+        private void TimerProcedure()
+        {
+            //wordt uitgevoerd in subthread
+            while (running)
+            {
+                Tick();
+            }
+        }
+
+        private void Tick()
+        {
+            Thread.Sleep(1000);
+            currentTime++;
+
+            OnTicked?.Invoke(this, currentTime);
+        }
+    }
+}
---
+++ ThreadExample/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CustomTimer myTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myTimer = new CustomTimer();
+            myTimer.OnTicked += MyTimer_Ticked;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myTimer.Start();
+        }
+
+        private void MyTimer_Ticked(object sender, int currentTime)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                lblTime.Content = currentTime;
+            }
+            else
+            {
+                //dispatch to MAIN thread
+                Dispatcher.Invoke(() => { MyTimer_Ticked(sender, currentTime); });
+            }
+            
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            myTimer.Stop();
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            myTimer.Stop();
+        }
+    }
+}
